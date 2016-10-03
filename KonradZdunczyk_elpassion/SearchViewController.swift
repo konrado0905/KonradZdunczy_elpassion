e0dc8f1ca08f95115dfc92846358f3780631ae3c
Konrad Zdunczyk <konrad.zdunczyk@gmail.com>
2016-10-03 21:50:30 +0200

    Bind SearchViewController with SearchViewModel

--- KonradZdunczyk_elpassion/SearchViewController.swift
+++ KonradZdunczyk_elpassion/SearchViewController.swift
@@ -7,19 +7,97 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class SearchViewController: UIViewController {
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView! {
         didSet {
             tableView.register(UserRepoCell.self, forCellReuseIdentifier: "cell")
+            tableView.dataSource = self
             tableView.keyboardDismissMode = .onDrag
         }
+    }
+
+    private let disposeBag = DisposeBag()
+    fileprivate var searchViewModel: SearchViewModel!
+
+    var rx_searchBarText: Observable<String> {
+        return searchBar.rx
+            .text
+            .throttle(1.0, scheduler: MainScheduler.instance)
+            .distinctUntilChanged()
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = "Search"
+        setupRx()
+    }
+
+    func setupRx() {
+        searchViewModel = SearchViewModel(withQueryOnservable: rx_searchBarText, errorHandler: showAlert)
+
+        searchBar.rx
+            .searchButtonClicked
+            .bindNext { [unowned self] in
+                self.view.endEditing(true)
+            }
+            .addDisposableTo(disposeBag)
+
+        searchViewModel
+            .rx_repoCellViewModels
+            .drive(onNext: { [unowned self] (repos) in
+                self.tableView.reloadData()
+            })
+            .addDisposableTo(disposeBag)
+
+        searchViewModel
+            .rx_userCellViewModels
+            .drive(onNext: { [unowned self] (users) in
+                self.tableView.reloadData()
+            })
+            .addDisposableTo(disposeBag)
+    }
+
+    private func showAlert(onError error: NSError) {
+        DispatchQueue.main.async {
+            guard self.presentedViewController == nil else { return }
+
+            let alertController = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+            let alertOkAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+
+            alertController.addAction(alertOkAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+    }
+}
+
+extension SearchViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case SearchViewModel.Consts.ReposSectionNumber:
+            return searchViewModel.repossCount
+        case SearchViewModel.Consts.UserSectionNumber:
+            return searchViewModel.usersCount
+        default:
+            return 0
+        }
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! UserRepoCell
+
+        if let viewModel = searchViewModel.getCellViewModel(forIndexPath: indexPath) {
+            cell.setup(withCellModelView: viewModel)
+        }
+
+        return cell
     }
 }
